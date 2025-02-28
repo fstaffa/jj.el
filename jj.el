20dd1c124087cb2103f40c1aa6f4bf84dee58292
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2025-02-28 21:44:30 +0100

    readme and simple status

---
+++ jj.el
@@ -0,0 +1,70 @@
+;;; jj.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Filip Staffa
+;;
+;; Author: Filip Staffa
+;; Maintainer: Filip Staffa
+;; Created: February 28, 2025
+;; Modified: February 28, 2025
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "28.1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'jj)
+
+
+(defun jj--get-project-folder ()
+  (locate-dominating-file default-directory ".jj" ))
+
+(defun jj--get-project-name ()
+  (-> (jj--get-project-folder)
+      file-name-directory
+      directory-file-name
+      file-name-nondirectory))
+
+(defun jj-status-mode--quit ()
+  (interactive)
+  (quit-window))
+
+(defvar jj-status-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "q") #'jj-status-mode--quit)
+    map))
+
+(define-derived-mode jj-status-mode special-mode "jj-status"
+  "Major mode for displaying Jujutsu status."
+  :group 'jj
+  :
+  (setq buffer-read-only t))
+
+
+(defun jj--run-command (command)
+  "Run a jj COMMAND from the project root"
+  (let* ((default-directory (jj--get-project-folder))
+         (jj-cmds (list "jj" "--no-pager" "--color" "never" command))
+         (cmd-string (s-join " " jj-cmds)))
+    (shell-command-to-string cmd-string)))
+
+(defun jj-status ()
+  "Display the status of the current jj repository."
+  (interactive)
+  (let ((buffer (get-buffer-create (format "jj: %s" (jj--get-project-name)))))
+    (with-current-buffer buffer
+      (let ((inhibit-read-only t))
+        (erase-buffer)
+        (insert (jj--run-command "st"))
+        (jj-status-mode)))
+    (switch-to-buffer buffer)))
+
+(evil-define-key 'normal jj-status-mode-map (kbd "q") #'jj-status-mode--quit)
+
+;;; jj.el ends here
