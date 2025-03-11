b5ba2de1cc3254038bd1dc14949690b5a2963698
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2025-03-11 20:21:20 +0100

    add logic to abandon whole branch (#1)

--- jj.el
+++ jj.el
@@ -7,7 +7,7 @@
 ;; Created: February 28, 2025
 ;; Modified: February 28, 2025
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "28.1"))
+;; Package-Requires: ((emacs "28.1") (s "1.13.0"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -19,8 +19,8 @@
 
 
 
+(require 's)
 (provide 'jj)
-(require 'transient)
 
 
 (defun jj--get-project-folder ()
@@ -44,12 +44,12 @@
   :
   (setq buffer-read-only t))
 
-
 (defun jj--run-command (command)
   "Run a jj COMMAND from the project root"
   (let* ((default-directory (jj--get-project-folder))
          (jj-cmds (list "jj" "--no-pager" "--color" "never" command))
          (cmd-string (s-join " " jj-cmds)))
+    (message cmd-string)
     (shell-command-to-string cmd-string)))
 
 (defun jj-status ()
@@ -63,12 +63,10 @@
         (jj-status-mode)))
     (switch-to-buffer buffer)))
 
-
 (defun jj-status-describe (args)
   "Run jj describe with ARGS."
   (interactive (list (transient-args 'jj-status-describe-popup)))
   (let* ((cmd (concat "describe " (string-join args " "))))
-    (message cmd)
     (jj--run-command cmd)
     (jj-status)))
 
@@ -79,11 +77,49 @@
   ["Actions"
    ("d" "Describe" jj-status-describe)])
 
+(defun jj--log-count-revs (revset)
+  "Count number of revisions in log."
+  (let ((log (jj--run-command (format "log -T '\"a\"' --revisions \"%s\" --no-graph" revset))))
+    (length log)))
+
+(defun jj--status-abandon-revset-from-trunk ()
+  "Prompt for branch name and create revset from trunk."
+  (interactive)
+  (let* ((jj-current-revset
+          (format "trunk()..%s"
+                  (jj--bookmarks-select)))
+         (revision-count (jj--log-count-revs jj-current-revset)))
+    (if (y-or-n-p (format "Abandon %d revisions? " revision-count))
+        (jj-status-abandon (append (list (format "\"%s\""  jj-current-revset)))))))
+
+(defun jj--bookmarks-get ()
+  "Get bookmarks from jj."
+  (let ((bookmarks (jj--run-command "bookmark list -T 'name ++ \"\n\"'")))
+    (s-split "\n" bookmarks 't)))
+
+(defun jj--bookmarks-select ()
+  "Select bookmark from jj."
+  (let ((bookmark (completing-read "Select branch: " (jj--bookmarks-get))))
+    bookmark))
+
+(defun jj-status-abandon (args)
+  "Run jj abandon with ARGS."
+  (interactive (list (transient-args 'jj-status-abandon-popup)))
+  (let* ((cmd (concat "abandon " (string-join args " "))))
+    (jj--run-command cmd)
+    (jj-status)))
+
+(transient-define-prefix jj-status-abandon-popup ()
+  "Popup for jujutsu abandon comand."
+  ["Actions"
+   ("b" "Abandon branch from trunk" jj--status-abandon-revset-from-trunk)])
+
 (transient-define-prefix jj-status-popup ()
   "Popup for jujutsu actions in the status buffer."
   ["Actions"
-   ("d" "Describe change" jj-status-describe-popup)]
-  )
+   ("d" "Describe change" jj-status-describe-popup)
+   ("a" "Abandon change" jj-status-abandon-popup)
+   ])
 
 (define-key jj-status-mode-map (kbd "q") #'jj-window-quit)
 (define-key jj-status-mode-map (kbd "?") #'jj-status-popup)
