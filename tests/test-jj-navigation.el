08cbdfe941835cbcefcd91d81dd2f0b124500b3b
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2025-10-18 13:31:23 +0200

    Enable jj-status-mode in navigation tests for Emacs 29.4 compatibility
    related to: https://github.com/mathematician314/jj.el/issues/10

--- tests/test-jj-navigation.el
+++ tests/test-jj-navigation.el
@@ -24,22 +24,24 @@
   (describe "jj-status--item-at-point"
     (it "should return file item when on a file line"
       (with-temp-buffer
+        (jj-status-mode)
         (let ((file-data '(:path "test.txt" :status "M"))
-              (start (point)))
+              (inhibit-read-only t))
           (insert "M  test.txt\n")
-          (put-text-property start (point) 'jj-item file-data)
-          (goto-char start)
+          (put-text-property 1 (1- (point-max)) 'jj-item file-data)
+          (goto-char 1)
           (let ((result (jj-status--item-at-point)))
             (expect (plist-get result :type) :to-be 'file)
             (expect (plist-get result :data) :to-equal file-data)))))
 
     (it "should return revision item when on a revision line"
       (with-temp-buffer
+        (jj-status-mode)
         (let ((rev-data '(:change-id "qpvuntsm" :description "Test"))
-              (start (point)))
+              (inhibit-read-only t))
           (insert "@  qpvuntsm  Test\n")
-          (put-text-property start (point) 'jj-item rev-data)
-          (goto-char start)
+          (put-text-property 1 (1- (point-max)) 'jj-item rev-data)
+          (goto-char 1)
           (let ((result (jj-status--item-at-point)))
             (expect (plist-get result :type) :to-be 'revision)
             (expect (plist-get result :data) :to-equal rev-data)))))
@@ -94,20 +96,22 @@
   (describe "jj-status-show-diff"
     (it "should show placeholder message for files"
       (with-temp-buffer
-        (let ((start (point)))
+        (jj-status-mode)
+        (let ((inhibit-read-only t))
           (insert "M  test.txt\n")
-          (put-text-property start (point) 'jj-item '(:path "test.txt" :status "M"))
-          (goto-char start)
+          (put-text-property 1 (1- (point-max)) 'jj-item '(:path "test.txt" :status "M"))
+          (goto-char 1)
           ;; Function shows message, doesn't throw error
           (jj-status-show-diff)
           (expect t :to-be t))))
 
     (it "should show placeholder message for revisions"
       (with-temp-buffer
-        (let ((start (point)))
+        (jj-status-mode)
+        (let ((inhibit-read-only t))
           (insert "@  qpvuntsm  Working copy\n")
-          (put-text-property start (point) 'jj-item '(:change-id "qpvuntsm"))
-          (goto-char start)
+          (put-text-property 1 (1- (point-max)) 'jj-item '(:change-id "qpvuntsm"))
+          (goto-char 1)
           ;; Function shows message, doesn't throw error
           (jj-status-show-diff)
           (expect t :to-be t))))))
